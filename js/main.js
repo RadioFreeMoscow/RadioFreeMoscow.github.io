2012f8e6295ad97f416926f46506a14b63db6c2b
RadioFreeMoscow <spozguy@gmail.com>
2017-07-16 17:01:52 +1000

    Settings now works

--- js/main.js
+++ js/main.js
@@ -3,16 +3,17 @@ $(document).ready(weatherSettings);
   function weatherSettings () {
     var config =
     {
-      url: 'https://api.openweathermap.org/forecast',
+      url: ' http://api.openweathermap.org/data/2.5/forecast',
       data: {
         id:'7281804',
         appid: '065e043f1d8cf7b05c1d84ea90c06987'
       }
     };
-    $.ajax(config).done(displayMelbourneData);
 
-    function displayMelbourneData(callback) {
-      console.log(callback);
+  $.ajax(config).done(displayMelbourneData);
+  console.log('Good');
+  function displayMelbourneData(callback) {
+      console.log('still good');
     }
 
   }
