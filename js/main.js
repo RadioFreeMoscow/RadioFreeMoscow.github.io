cd7a3f7ce98b856d2809e9c63235c207552499a0
RadioFreeMoscow <spozguy@gmail.com>
2017-07-17 11:40:31 +1000

    Playing with if Statements

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-console.log('Connected to Script.js');
 $(document).ready(weatherSettings);
+
   function weatherSettings () {
     var config =
     {
-      url: ' http://api.openweathermap.org/data/2.5/forecast',
+      url: ' http://api.openweathermap.org/data/2.5/weather',
       data: {
         id:'7281804',
         appid: '065e043f1d8cf7b05c1d84ea90c06987'
@@ -13,8 +13,14 @@ $(document).ready(weatherSettings);
   $.ajax(config).done(displayMelbourneData);
   console.log('Good');
 
+  }
   function displayMelbourneData(callback) {
+    console.log(callback);
+    var tempCel = callback.main.temp - 273.15;
+    console.log(tempCel.toFixed(2));
+    if (tempCel > 10) {
+      $('.weather').addClass('flaticon-021-snowflake').text(tempCel.toFixed(2));
+    } else
+    $('.weather').text(tempCel.toFixed(2));
 
     }
-
-  }
